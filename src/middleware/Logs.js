d8abda4fb57816a9bbe432a3d563a9a7a2ef3432
Timson020 <a_sa_f@126.com>
2018-08-01 11:16:18 +0800

    add the model for user action

--- src/middleware/Logs.js
+++ src/middleware/Logs.js
@@ -1,8 +1,22 @@
+import ActionsSchema from '../model/Actions'
 import { Constants } from '../common'
 
 export default function log(req, res, next) {
+	const { ip, url, method, headers, params, body: bodys } = req
+
+	const header = JSON.stringify(headers)
+
+	const body = JSON.stringify(bodys)
+
+	const params = JSON.stringify(params)
+
+	const action = new ActionsSchema({ ip, url, method, header, body })
+	
+	action.save((err) => {
+		if (err) return res.t_log.error(`${Constants.serverMiddleWare}time = ${res.t_dateFormat()}, statusCode = 500, errMsg = ${err.toString()}`)
+	})
 	// 打印 请求最开始的日志
-	res.t_log.info(`${Constants.serverStart}time = ${res.t_dateFormat()}, method = ${req.method}, url = ${req.url}, ip = ${req.ip}, params = ${JSON.stringify(req.params)}, body = ${JSON.stringify(req.body)}`)
+	res.t_log.info(`${Constants.serverStart}time = ${res.t_dateFormat()}, ip = ${ip}, url = ${url}, method = ${method}, params = ${params}, body = ${body}`)
 
 	// respond结束的监听事件
 	res.on('finish', () => {
---
+++ src/model/Actions.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import db from './index'
+db
+
+const Schema = new mongoose.Schema({
+	ip: { type: String, default: '' }, // request from ip
+	url: { type: String, default: '' }, // action-url
+	method: { type: String, default: '' }, // 方法
+	header: { type: String, default: '' }, // url-header
+	body: { type: String, default: '' }, // 内容
+	create_at: { type: Date, default: Date() }, // 创建时间
+})
+
+export default mongoose.model('User', Schema)
