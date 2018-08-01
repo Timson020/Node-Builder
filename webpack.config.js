f56196df95a1ac8266970e5ca76eb6e4046df100
Timson020 <a_sa_f@126.com>
2018-08-01 09:54:56 +0800

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,53 @@
 const path = require('path')
 const webpack = require('webpack')
-const _externals = require('externals-dependencies')
+// const _externals = require('externals-dependencies')
 
 const isDev = process.env.NODE_ENV === 'development'
 
-// 默认配置
-const defaultconfig = {
+const config = {
+	name: "server",
 	target: 'node',
+	mode: 'production',
 	devtool: '#eval-source-map',
-	context: __dirname,
+	entry: {
+		app: ['babel-polyfill', './index.js'],
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: './dist',
+		filename: '[name].js?[hash]',
+	},
 	resolve: {
 		extensions: ['.js', '.json'],
 		alias: {
 			'@': path.resolve(__dirname, './src')
 		}
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			use: 'babel-loader',
+			exclude: /node_modules/,
+		}],
+	},
+	plugins: [
+		// 插件中的全局变量 -- 4.x 可以弃用
+		// new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' } }),
+		// loder里面的配置 -- 4.x 可以弃用
+		// new webpack.LoaderOptionsPlugin({ minimize: true }),
+	],
+	performance: {
+		hints: false,
+	},
+	node: {
+		__filename: true,
+		__dirname: true,
+		console: true,
+		global: true,
+		process: true,
+		Buffer: true,
+		setImmediate: true,
+		path: true
 	},
 	stats: {
 		cached: true,
@@ -28,48 +62,5 @@ const defaultconfig = {
 		version: true,
 	},
 }
-
-// 生产环境打包配置
-const releaseconfig = {
-	name: "server",
-	mode: 'production',
-	devtool: '',
-	entry: {
-		app: ['babel-polyfill', './index.js'],
-	},
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		publicPath: './dist',
-		filename: '[name].js?[hash]',
-	},
-	node: {
-		console: true,
-		global: true,
-		process: true,
-		Buffer: true,
-		__filename: true,
-		__dirname: true,
-		setImmediate: true,
-		path: true
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			use: 'babel-loader',
-			exclude: /node_modules/,
-		}],
-	},
-	plugins: [
-		// 插件中的全局变量
-		new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' } }),
-		// loder里面的配置
-		new webpack.LoaderOptionsPlugin({ minimize: true }),
-	],
-	performance: {
-		hints: false,
-	},
-}
-
-const config = Object.assign(defaultconfig, releaseconfig)
 
 module.exports = config
